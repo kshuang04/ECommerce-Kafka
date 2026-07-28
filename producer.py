5e1508ac9aa37ae3b1056f60658fda616b6a1c34
kshuang04 <kitkatpanda8@gmail.com>
2026-07-28 15:54:22 -0700

    Encapsulation

--- producer.py
+++ producer.py
@@ -5,17 +5,39 @@ import uuid
 
 from confluent_kafka import Producer
 
-producer_config = {
-    "bootstrap.servers": "localhost:9092",
-}
+def main():
+    producer_config = {
+        "bootstrap.servers": "localhost:9092",
+    }
 
-producer = Producer(producer_config)
+    producer = Producer(producer_config)
+
+    try:
+        while True:
+            order = generate_order()
+            value = json.dumps(order).encode("utf-8")
+
+            producer.produce(
+                topic="orders",
+                value=value,
+                callback=transaction_report
+            )
+
+            producer.poll(0)
+
+            time.sleep(2)
+
+    except KeyboardInterrupt:
+        print("\n🔴 Stopping producer...")
+
+    finally:
+        producer.flush()
 
 def transaction_report(err, msg):
     if err:
         print(f"❌ Transaction failed: {err}")
     else:
-        print(f"✅ Transaction sent to {msg.topic()} : partition {msg.partition()} : at offset {msg.offset()}")
+        print(f"✅ Transaction details sent to {msg.topic()} : partition {msg.partition()} : at offset {msg.offset()}")
 
 def generate_order():
     items = ["keyboard", "mouse", "monitor", "cables", "ergonomic chair"]
@@ -28,23 +50,5 @@ def generate_order():
         "user": random.choice(users),
     }
 
-try:
-    while True:
-        order = generate_order()
-        value = json.dumps(order).encode("utf-8")
-
-        producer.produce(
-            topic="orders",
-            value=value,
-            callback=transaction_report
-        )
-
-        producer.poll(0)
-
-        time.sleep(2)
-
-except KeyboardInterrupt:
-    print("\n🔴 Stopping producer...")
-
-finally:
-    producer.flush()
+if __name__ == "__main__":
+    main()
